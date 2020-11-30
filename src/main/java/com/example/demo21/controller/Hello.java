aef7e7eb896f752e57fe37d1ab68a10866967da6
kathirms <kathirvel.m@batonsystems>
2020-11-30 14:08:34 +0530

    this me demo21

---
+++ src/main/java/com/example/demo21/controller/Hello.java
@@ -0,0 +1,12 @@
+package com.example.demo21.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping(value = "/hello")
+    public String getString(){
+        return "Hello World";
+    }
+}
